8c568e58e2dd2bff757eb15aff1afe7bb653fcff
Zifan Gao <zifan.gao.jrvs.ca@gmail.com>
2019-06-24 18:35:22 +0000

    edited to pass test

--- solutions.sql
+++ solutions.sql
@@ -1,8 +1,8 @@
-SELECT cpu_number, host_id, total_mem from host_info
-GROUP BY cpu_number
+SELECT cpu_number, id as host_id, total_mem from host_info
+GROUP BY id
 ORDER BY total_mem DESC;
 
-SELECT host_info.host_id, host_info.host_name, host_info.total_mem as total_memory, AVG(host_info.total_mem - host_usage.memory_free) as used_memory_percentage, (date_trunc('hour', timestamp) + date_part('minute', timestamp)::int / 5 * interval '5 min') as 5_min_timestamp
+SELECT host_info.id as host_id, host_info.hostname, host_info.total_mem as total_memory, AVG(host_info.total_mem - host_usage.memory_free) as used_memory_percentage, (date_trunc('hour', host_info."timestamp") + date_part('minute',host_info."timestamp")::int / 5 * interval '5 min') as _5min_timestamp
 FROM host_info
-INNER JOIN host_usage ON host_info.timestamp = host_usage.timestamp 
-GROUP BY 5_min_timestamp;
+INNER JOIN host_usage ON host_info.id = host_usage.host_id 
+GROUP BY _5min_timestamp, id;
